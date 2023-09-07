a34d08fd8c4339242ae4e169b8c815e89720f3b2
Bruno Muniz <brunomunizdev@gmail.com>
2023-09-07 12:56:48 -0300

    Criar controller CoinViewController e adicionar navegação entre as telas

---
+++ exchangeInCoins/CoinViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CoinViewController.swift
+//  exchangeInCoins
+//
+//  Created by Bruno Teixeira Muniz Rocha on 07/09/23.
+//
+
+import UIKit
+
+class CoinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurarLayout()
+    }
+    
+    func configurarLayout() {
+        //navigationItem.hidesBackButton = true
+    }
+
+}
